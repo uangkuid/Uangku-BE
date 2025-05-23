d4a16775b809213a44c9aa173443ac6f45af20ea
Oratakashi Nhamako <oratakashi@oratakashi.com>
2025-05-23 08:50:41 +0700

    [UK-88] chore: add feature status table migration

---
+++ database/migrations/2025_05_23_013151_create_feature_statuses_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('feature_statuses', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('feature_name')->unique();
+            $table->boolean('is_enabled')->default(true);
+            $table->uuid('updated_by')->nullable();
+            $table->timestamp('updated_at')->useCurrent();
+            $table->foreign('updated_by')->references('id')->on('staff_accounts')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('feature_statuses');
+    }
+};
