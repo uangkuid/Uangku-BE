c0f6d346ebb43c3bafb42492291c56881dd147cc
Oratakashi Nhamako <oratakashi@oratakashi.com>
2025-05-23 08:55:56 +0700

    [UK-89] chore: add system config table migration

---
+++ database/migrations/2025_05_23_012952_create_system_configs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('system_configs', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('key')->unique();
+            $table->text('value');
+            $table->uuid('updated_by')->nullable();
+            $table->timestamp('updated_at')->useCurrent();
+            $table->foreign('updated_by')->references('id')->on('staff_accounts')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('system_configs');
+    }
+};
