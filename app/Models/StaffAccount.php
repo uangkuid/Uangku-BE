5e3a4cad4933dfe7fa27212b19a1ecb7e0da4c7c
Oratakashi Nhamako <oratakashi@oratakashi.com>
2025-05-24 15:07:19 +0700

    [UK-95] chore: add model for staff account

--- app/Models/StaffAccount.php
+++ app/Models/StaffAccount.php
@@ -2,9 +2,55 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class StaffAccount extends Model
+class StaffAccount extends Authenticatable implements JWTSubject
 {
-    //
+    use HasFactory, Notifiable, HasUuids;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+    ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [
+            "data" => [
+                "staff_id" => $this->id
+            ]
+        ];
+    }
 }
--- app/Models/StaffKey.php
+++ app/Models/StaffKey.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class StaffKey extends Model
 {
-    //
+
+    use HasFactory, HasUuids;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'staffs',
+        'public_key',
+        'private_key',
+        'hashed_key',
+        'hashed_pin',
+    ];
 }
