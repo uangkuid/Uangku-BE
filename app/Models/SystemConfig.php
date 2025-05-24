a785e1fbc12bf868c9fe9a10f498e1779c87f12f
Oratakashi Nhamako <oratakashi@oratakashi.com>
2025-05-24 15:19:31 +0700

    [UK-89] chore: add model for system config

--- app/Models/SystemConfig.php
+++ app/Models/SystemConfig.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SystemConfig extends Model
 {
-    //
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'updated_by',
+        'key',
+        'value',
+    ];
 }
