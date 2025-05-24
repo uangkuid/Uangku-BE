6b57f9a1f7de662aaa56299253798d2af4610882
Oratakashi Nhamako <oratakashi@oratakashi.com>
2025-05-24 15:14:51 +0700

    [UK-88] chore: add model for feature status

--- app/Models/FeatureStatus.php
+++ app/Models/FeatureStatus.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class FeatureStatus extends Model
 {
-    //
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'feature_name',
+        'is_enabled',
+        'updated_by',
+    ];
 }
