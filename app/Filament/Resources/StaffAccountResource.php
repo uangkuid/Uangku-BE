e19f898ec774f2af7159a7a9a0f84c054494c6aa
Oratakashi Nhamako <davidcreazy@gmail.com>
2025-05-29 23:02:40 +0700

    [UK-92] adjustment: Staff Account Form

--- app/Filament/Resources/StaffAccountResource.php
+++ app/Filament/Resources/StaffAccountResource.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\StaffAccountResource\Pages;
 use App\Filament\Resources\StaffAccountResource\RelationManagers;
+use App\Models\Shop\Order;
 use App\Models\StaffAccount;
 use Filament\Forms;
 use Filament\Forms\Components\Select;
@@ -14,8 +15,6 @@ use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Illuminate\Support\Facades\Hash;
 
 class StaffAccountResource extends Resource
@@ -29,39 +28,62 @@ class StaffAccountResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->required()
-                    ->maxLength(255)
-                    ->columnSpanFull(),
-                Forms\Components\TextInput::make('email')
-                    ->email()
-                    ->required()
-                    ->maxLength(255)
-                    ->columnSpanFull(),
-                Select::make('role')
-                    ->options([
-                        'member' => 'Member',
-                        'admin' => 'Admin',
+                Forms\Components\Group::make()
+                    ->schema([
+                        Forms\Components\Section::make('Staff Information')
+                            ->schema([
+                                Forms\Components\TextInput::make('name')
+                                    ->required()
+                                    ->maxLength(255)
+                                    ->columnSpanFull(),
+                                Forms\Components\TextInput::make('email')
+                                    ->email()
+                                    ->required()
+                                    ->maxLength(255)
+                                    ->columnSpanFull(),
+                                Select::make('role')
+                                    ->options([
+                                        'member' => 'Member',
+                                        'admin' => 'Admin',
+                                    ])
+                                    ->required()
+                                    ->columnSpanFull(),
+                            ]),
+                        Forms\Components\Section::make('Security Settings')
+                            ->schema([
+                                TextInput::make('password')
+                                    ->password()
+                                    ->revealable()
+                                    ->required(fn(string $context): bool => $context === 'create')
+                                    ->maxLength(255)
+                                    ->dehydrateStateUsing(fn(string $state): string => Hash::make($state))
+                                    ->dehydrated(fn(?string $state): bool => filled($state))
+                                    ->columnSpanFull(),
+                                TextInput::make('password_confirmation')
+                                    ->password()
+                                    ->required(fn(string $context): bool => $context === 'create')
+                                    ->revealable()
+                                    ->maxLength(255)
+                                    ->same('password')
+                                    ->label('Confirm Password')
+                                    ->columnSpanFull(),
+                            ])
                     ])
-                    ->required()
-                    ->columnSpanFull(),
-                TextInput::make('password')
-                    ->password()
-                    ->revealable()
-                    ->required(fn (string $context): bool => $context === 'create')
-                    ->maxLength(255)
-                    ->dehydrateStateUsing(fn (string $state): string => Hash::make($state))
-                    ->dehydrated(fn (?string $state): bool => filled($state))
-                    ->columnSpanFull(),
-                TextInput::make('password_confirmation')
-                    ->password()
-                    ->required(fn (string $context): bool => $context === 'create')
-                    ->revealable()
-                    ->maxLength(255)
-                    ->same('password')
-                    ->label('Confirm Password')
-                    ->columnSpanFull(),
-            ]);
+                    ->columnSpan(['lg' => fn(?StaffAccount $record) => $record === null ? 3 : 2]),
+                Forms\Components\Section::make('General Information')
+                    ->schema([
+                        Forms\Components\Placeholder::make('created_at')
+                            ->label('Created at')
+                            ->content(fn(StaffAccount $record): ?string => $record->created_at?->format('d M Y - H:i:s')),
+
+                        Forms\Components\Placeholder::make('updated_at')
+                            ->label('Last modified at')
+                            ->content(fn(StaffAccount $record): ?string => $record->updated_at?->diffForHumans()),
+                    ])
+                    ->columnSpan(['lg' => 1])
+                    ->hidden(fn(?StaffAccount $record) => $record === null),
+            ])
+            ->columns(3);
     }
 
     public static function table(Table $table): Table
