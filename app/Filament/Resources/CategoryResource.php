0d73ef901b97a0825bb1d2b49e60531de14140b1
Oratakashi Nhamako <davidcreazy@gmail.com>
2025-05-29 23:11:54 +0700

    draft: Category dashboard

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -14,7 +14,6 @@ use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Illuminate\Support\Facades\Storage;
 
 class CategoryResource extends Resource
@@ -36,7 +35,7 @@ class CategoryResource extends Resource
                     ->default(null),
                 Forms\Components\TextInput::make('transaction_types')
                     ->required()
-                    ->maxLength(36),
+                    ->relationship('transactionType', 'name')
             ]);
     }
 
