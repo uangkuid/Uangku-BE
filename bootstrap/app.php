42ffcb585a91fefc954e26378f72f035e7c5c70e
Oratakashi Nhamako <davidcreazy@gmail.com>
2026-07-09 21:12:35 +0700

    fix: trust all proxies in middleware configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,6 +22,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+
         $middleware->appendToGroup('family', [
             FamilyMiddleware::class,
         ]);
